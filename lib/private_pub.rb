ce520dceda90f6ad288439da3cc2b872c46c1882
myupchar <35686264+myupchar@users.noreply.github.com>
2018-03-03 12:57:21 +0530

    Updating timestamp to solve  issues with mobile communication

--- lib/private_pub.rb
+++ lib/private_pub.rb
@@ -56,7 +56,7 @@ module PrivatePub
     # Returns a subscription hash to pass to the PrivatePub.sign call in JavaScript.
     # Any options passed are merged to the hash.
     def subscription(options = {})
-      sub = {:server => config[:server], :timestamp => (Time.now.to_f * 1000).round}.merge(options)
+      sub = {:server => config[:server], :timestamp => (Time.now.to_i/100000).round}.merge(options)
       sub[:signature] = Digest::SHA1.hexdigest([config[:secret_token], sub[:channel], sub[:timestamp]].join)
       sub
     end
